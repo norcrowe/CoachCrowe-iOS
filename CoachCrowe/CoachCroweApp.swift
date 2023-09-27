4cc54989141fc9db02dd440159c849ff63f96078
NorCrowE <norcrowe2@gmail.com>
2023-09-27 13:58:31 +0800

    Initial commit

---
+++ CoachCrowe/CoachCroweApp.swift
@@ -0,0 +1,15 @@
+//  CoachCroweApp.swift
+//  Created by nor
+//  This one for CR7
+
+
+import SwiftUI
+
+@main
+struct CoachCroweApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CoachCrowe/ContentView.swift
@@ -0,0 +1,14 @@
+//
+//  ContentView.swift
+//  CoachCrowe
+//
+//  Created by Nor CrowE on 2023/9/27.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+    }
+}
